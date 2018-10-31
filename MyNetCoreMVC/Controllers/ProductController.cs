7ad42fad0960497c34d9c02d7b8b89bc7048c90f
hanhnt2912 <hanhntd00589@fpt.edu.vn>
2018-11-01 00:12:53 +0700

    first

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MyNetCoreMVC.Models;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyDbContact _context;
+       
+
+        public ProductController(MyDbContact context)
+        {
+            _context = context;
+        }
+       
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Update(Product product)
+        {
+            var exisProduct = _context.Products.Find(product.Id);
+            if (exisProduct == null)
+            {
+                return NotFound();
+            }
+
+            exisProduct.Name = product.Name;
+            exisProduct.Price = product.Price;
+            _context.Products.Update(exisProduct);
+            _context.SaveChanges();
+            return Redirect("Index");
+
+        }
+
+        
+        
+            public IActionResult Edit(long id)
+            {
+                var product = _context.Products.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+
+        
+
+
+        public IActionResult Delete(long id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return View("Index");
+
+        }
+
+        public IActionResult Save(MyDbContact product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Redirect("Index");
+        } 
+
+    }
+}
---
+++ MyNetCoreMVC/Models/MyDbContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyNetCoreMVC.Models
+{
+    public class MyDbContact : DbContext
+    {
+        public MyDbContact(DbContextOptions<MyDbContact> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyDbContact> Products { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        internal object Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
